de8afef6082b21f55d46de62650bd8ac50ac3ac7
Juliocsant <juliocesar_san@outlook.com>
2024-12-11 17:10:26 -0300

    desquebrou

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,9 +2,6 @@ from django.conf import settings
 from django.db import models
 
 
-from django.db import models
-from django.conf import settings
-
 
 from django.conf import settings
 from django.db import models
@@ -20,12 +17,11 @@ class ChatHistory(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        null=True,
-        blank=True  # Certifique-se de que há uma vírgula no final da linha anterior
+        null=True, blank=True
     )
     question = models.TextField()
     answer = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user} - {self.question[:20]}"
+        return f"Usuário: {self.user} - Pergunta: {self.question[:20]}"
